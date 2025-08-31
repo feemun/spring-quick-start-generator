ec2dae7ce187800b6f322cb4f55ec7b8cc9284f3
feemun <31443333+feemun@users.noreply.github.com>
2025-08-31 22:40:27 +0800

    更新模板

--- common/src/main/java/cloud/catfish/api/CommonPage.java
+++ common/src/main/java/cloud/catfish/api/CommonPage.java
@@ -39,11 +39,11 @@ public class CommonPage<T> {
     public static <T> CommonPage<T> restPage(List<T> list) {
         CommonPage<T> result = new CommonPage<T>();
         PageInfo<T> pageInfo = new PageInfo<T>(list);
-        result.setTotalPage(pageInfo.getPages());
-        result.setPageNum(pageInfo.getPageNum());
-        result.setPageSize(pageInfo.getPageSize());
-        result.setTotal(pageInfo.getTotal());
-        result.setList(pageInfo.getList());
+//        result.setTotalPage(pageInfo.getPages());
+//        result.setPageNum(pageInfo.getPageNum());
+//        result.setPageSize(pageInfo.getPageSize());
+//        result.setTotal(pageInfo.getTotal());
+//        result.setList(pageInfo.getList());
         return result;
     }
 
@@ -52,11 +52,11 @@ public class CommonPage<T> {
      */
     public static <T> CommonPage<T> restPage(Page<T> pageInfo) {
         CommonPage<T> result = new CommonPage<T>();
-        result.setTotalPage(pageInfo.getTotalPages());
-        result.setPageNum(pageInfo.getNumber());
-        result.setPageSize(pageInfo.getSize());
-        result.setTotal(pageInfo.getTotalElements());
-        result.setList(pageInfo.getContent());
+//        result.setTotalPage(pageInfo.getTotalPages());
+//        result.setPageNum(pageInfo.getNumber());
+//        result.setPageSize(pageInfo.getSize());
+//        result.setTotal(pageInfo.getTotalElements());
+//        result.setList(pageInfo.getContent());
         return result;
     }
 
