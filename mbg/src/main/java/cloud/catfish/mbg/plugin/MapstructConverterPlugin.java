33f208c906190a1d7fc898ef3a21b4776eeea8d2
feemun <31443333+feemun@users.noreply.github.com>
2025-10-16 13:58:09 +0800

    refactor vo and converter

---
+++ mbg/src/main/java/cloud/catfish/mbg/plugin/MapstructConverterPlugin.java
@@ -0,0 +1,129 @@
+package cloud.catfish.mbg.plugin;
+
+import cloud.catfish.mbg.comm.CommonConstant;
+import cloud.catfish.mbg.util.VelocityUtil;
+import org.mybatis.generator.api.IntrospectedTable;
+import org.mybatis.generator.api.PluginAdapter;
+import org.mybatis.generator.api.dom.java.TopLevelClass;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.StringWriter;
+import java.util.List;
+import java.util.Properties;
+
+public class MapstructConverterPlugin extends PluginAdapter {
+
+    // MapStruct annotation
+    private static final String MAPSTRUCT_MAPPER_CLASS = "org.mapstruct.Mapper";
+
+    @Override
+    public boolean validate(List<String> warnings) {
+        return true;
+    }
+
+    @Override
+    public boolean modelBaseRecordClassGenerated(TopLevelClass topLevelClass, IntrospectedTable introspectedTable) {
+        try {
+            // Generate MapStruct converter
+            generateMapStructConverter(topLevelClass, introspectedTable);
+        } catch (Exception e) {
+            System.err.println("Warning: Failed to generate Converter for " + topLevelClass.getType().getShortName() + ": " + e.getMessage());
+        }
+
+        return super.modelBaseRecordClassGenerated(topLevelClass, introspectedTable);
+    }
+
+    /**
+     * Generates a MapStruct converter interface for domain-VO conversion.
+     */
+    private void generateMapStructConverter(TopLevelClass domainClass, IntrospectedTable introspectedTable) throws IOException {
+        String entityName = domainClass.getType().getShortName();
+
+        String modelClassName = entityName;
+        String voClassName = entityName + CommonConstant.VO_SUFFIX_FILE_NAME;
+        String converterClassName = entityName + CommonConstant.REQUEST_SUFFIX_PARAM_FILE_NAME;
+
+        StringBuilder converterContent = new StringBuilder();
+
+        // Package declaration
+        converterContent.append("package ")
+                .append(CommonConstant.CONVERTER_PACKAGE_NAME)
+                .append(";\n\n");
+
+        // Imports
+        converterContent.append("import ")
+                .append(MAPSTRUCT_MAPPER_CLASS)
+                .append(";\n");
+        converterContent.append("import ")
+                .append(domainClass.getType().getFullyQualifiedName())
+                .append(";\n");
+        converterContent.append("import ")
+                .append(CommonConstant.VO_PACKAGE_NAME)
+                .append(".")
+                .append(entityName + CommonConstant.VO_SUFFIX_FILE_NAME)
+                .append(";\n");
+        converterContent.append("import java.util.List;\n\n");
+
+        // Converter annotation and interface declaration
+        converterContent.append("@Mapper(componentModel = \"")
+                .append("spring")
+                .append("\")\n");
+        converterContent.append("public interface ")
+                .append(converterClassName)
+                .append(" {\n\n");
+
+        // Conversion methods
+        converterContent.append("    /**\n");
+        converterContent.append("     * Converts a domain model to a VO.\n");
+        converterContent.append("     * \n");
+        converterContent.append("     * @param ").append(modelClassName.toLowerCase()).append(" the domain model to convert\n");
+        converterContent.append("     * @return the corresponding VO, or null if input is null\n");
+        converterContent.append("     */\n");
+        converterContent.append("    ").append(voClassName).append(" toVo(").append(modelClassName).append(" ").append(modelClassName.toLowerCase()).append(");\n\n");
+
+        converterContent.append("    /**\n");
+        converterContent.append("     * Converts a VO to a domain model.\n");
+        converterContent.append("     * \n");
+        converterContent.append("     * @param ").append(voClassName.toLowerCase()).append(" the VO to convert\n");
+        converterContent.append("     * @return the corresponding domain model, or null if input is null\n");
+        converterContent.append("     */\n");
+        converterContent.append("    ").append(modelClassName).append(" toDomain(").append(voClassName).append(" ").append(voClassName.toLowerCase()).append(");\n\n");
+
+        converterContent.append("    /**\n");
+        converterContent.append("     * Converts a list of domain models to VOs.\n");
+        converterContent.append("     * \n");
+        converterContent.append("     * @param ").append(modelClassName.toLowerCase()).append("s the list of domain models to convert\n");
+        converterContent.append("     * @return the list of corresponding VOs, or null if input is null\n");
+        converterContent.append("     */\n");
+        converterContent.append("    List<").append(voClassName).append("> toVoList(List<").append(modelClassName).append("> ").append(modelClassName.toLowerCase()).append("s);\n\n");
+
+        converterContent.append("    /**\n");
+        converterContent.append("     * Converts a list of VOs to domain models.\n");
+        converterContent.append("     * \n");
+        converterContent.append("     * @param ").append(voClassName.toLowerCase()).append("s the list of VOs to convert\n");
+        converterContent.append("     * @return the list of corresponding domain models, or null if input is null\n");
+        converterContent.append("     */\n");
+        converterContent.append("    List<").append(modelClassName).append("> toDomainList(List<").append(voClassName).append("> ").append(voClassName.toLowerCase()).append("s);\n");
+
+        converterContent.append("}\n");
+
+        // Write converter file
+        writeConverterFile(converterContent.toString(), converterClassName);
+    }
+
+    /**
+     * Writes the MapStruct converter interface to a file.
+     */
+    private void writeConverterFile(String content, String className) throws IOException {
+        StringWriter writer = new StringWriter();
+        writer.write(content);
+        VelocityUtil.processTemplate(writer,
+                CommonConstant.MBG_MODULE_ABSOLUTE_PATH + CommonConstant.RESOURCES_RELATIVE_PATH + CommonConstant.CONTROLLER_PACKAGE_RELATIVE_NAME,
+                className + ".java"
+        );
+        System.out.println("Generated Converter: " + className + ".java");
+    }
+
+}
---
+++ mbg/src/main/java/cloud/catfish/mbg/plugin/VoRecordPlugin.java
@@ -0,0 +1,164 @@
+package cloud.catfish.mbg.plugin;
+
+import cloud.catfish.mbg.comm.CommonConstant;
+import cloud.catfish.mbg.util.VelocityUtil;
+import org.mybatis.generator.api.IntrospectedColumn;
+import org.mybatis.generator.api.IntrospectedTable;
+import org.mybatis.generator.api.PluginAdapter;
+import org.mybatis.generator.api.dom.java.Field;
+import org.mybatis.generator.api.dom.java.FullyQualifiedJavaType;
+import org.mybatis.generator.api.dom.java.TopLevelClass;
+
+import java.io.StringWriter;
+import java.util.List;
+import java.util.Properties;
+
+public class VoRecordPlugin extends PluginAdapter {
+
+    // Swagger3 annotations
+    private static final String SWAGGER_SCHEMA_CLASS = "io.swagger.v3.oas.annotations.media.Schema";
+
+    // Constants for JSON and DateTime format annotations
+    private static final String JSON_FORMAT_CLASS = "com.fasterxml.jackson.annotation.JsonFormat";
+    private static final String DATETIME_FORMAT_CLASS = "org.springframework.format.annotation.DateTimeFormat";
+    private static final String LOCALDATETIME_TYPE = "LocalDateTime";
+
+    @Override
+    public boolean validate(List<String> warnings) {
+        return true;
+    }
+
+    @Override
+    public boolean modelBaseRecordClassGenerated(TopLevelClass topLevelClass, IntrospectedTable introspectedTable) {
+        generateVoRecord(topLevelClass, introspectedTable);
+        return super.modelBaseRecordClassGenerated(topLevelClass, introspectedTable);
+    }
+
+    /**
+     * Generates a VO record class based on the domain model.
+     */
+    private void generateVoRecord(TopLevelClass domainClass, IntrospectedTable introspectedTable) {
+        String entityName = domainClass.getType().getShortName();
+
+        String modelClassName = entityName;
+        String voClassName = entityName + CommonConstant.VO_SUFFIX_FILE_NAME;
+        String converterClassName = entityName + CommonConstant.REQUEST_SUFFIX_PARAM_FILE_NAME;
+
+        StringBuilder voContent = new StringBuilder();
+
+        // Package declaration
+        voContent.append("package ")
+                .append(CommonConstant.VO_PACKAGE_NAME)
+                .append(";\n\n");
+
+        // Imports
+        addImports(voContent, domainClass);
+
+        // Swagger annotation for class
+        voContent.append("@Schema(description = \"")
+                .append(entityName)
+                .append("\")\n");
+
+        // Record declaration
+        voContent.append("public record ").append(voClassName).append("(\n");
+
+        // Record components (fields)
+        List<Field> fields = domainClass.getFields();
+        for (int i = 0; i < fields.size(); i++) {
+            Field field = fields.get(i);
+
+            // Add JSON format and DateTime format annotations for LocalDateTime fields
+            if (LOCALDATETIME_TYPE.equals(field.getType().getShortName())) {
+                voContent.append("    @JsonFormat(pattern = \"yyyy-MM-dd HH:mm:ss\", timezone = \"GMT+8\")\n");
+                voContent.append("    @DateTimeFormat(pattern = \"yyyy-MM-dd HH:mm:ss\")\n");
+            }
+
+            // Add Swagger annotation for field
+            voContent.append("    @Schema(description = \"").append(getFieldDescription(field, introspectedTable)).append("\")\n");
+
+            voContent.append("    ").append(field.getType().getShortName()).append(" ").append(field.getName());
+            if (i < fields.size() - 1) {
+                voContent.append(",");
+            }
+            voContent.append("\n");
+        }
+
+        voContent.append(") {}\n");
+
+        // Write VO file
+        writeVoFile(voContent.toString(), voClassName);
+    }
+
+    /**
+     * Adds necessary imports to the VO class.
+     */
+    private void addImports(StringBuilder content, TopLevelClass domainClass) {
+        // Check if LocalDateTime fields exist
+        boolean hasLocalDateTime = domainClass.getFields().stream()
+                .anyMatch(field -> LOCALDATETIME_TYPE.equals(field.getType().getShortName()));
+
+        // Add JSON format and DateTime format imports if LocalDateTime fields exist
+        if (hasLocalDateTime) {
+            content.append("import ").append(JSON_FORMAT_CLASS).append(";\n");
+            content.append("import ").append(DATETIME_FORMAT_CLASS).append(";\n");
+        }
+
+        // Always add Swagger import
+        content.append("import ").append(SWAGGER_SCHEMA_CLASS).append(";\n");
+
+        // Collect unique import types from fields
+        domainClass.getFields().stream()
+                .map(Field::getType)
+                .filter(type -> !type.getPackageName().equals("java.lang") && type.getPackageName() != null)
+                .map(FullyQualifiedJavaType::getFullyQualifiedName)
+                .distinct()
+                .sorted()
+                .forEach(importName -> content.append("import ").append(importName).append(";\n"));
+
+        if (!domainClass.getFields().isEmpty()) {
+            content.append("\n");
+        }
+    }
+
+    /**
+     * Generates a description for a field based on database column comments.
+     */
+    private String getFieldDescription(Field field, IntrospectedTable introspectedTable) {
+        String fieldName = field.getName();
+
+        // Try to find the corresponding column comment from the database
+        for (IntrospectedColumn column : introspectedTable.getAllColumns()) {
+            if (column.getJavaProperty().equals(fieldName)) {
+                String remarks = column.getRemarks();
+                if (remarks != null && !remarks.trim().isEmpty()) {
+                    return remarks.trim();
+                }
+                break;
+            }
+        }
+
+        // Fallback to generated description if no column comment is available
+        String readableName = fieldName.replaceAll("([a-z])([A-Z])", "$1 $2").toLowerCase();
+
+        // Capitalize first letter
+        if (!readableName.isEmpty()) {
+            readableName = Character.toUpperCase(readableName.charAt(0)) + readableName.substring(1);
+        }
+
+        return readableName;
+    }
+
+    /**
+     * Writes the VO record class to a file.
+     */
+    private void writeVoFile(String content, String className) {
+        StringWriter writer = new StringWriter();
+        writer.write(content);
+        VelocityUtil.processTemplate(writer,
+                CommonConstant.MBG_MODULE_ABSOLUTE_PATH + CommonConstant.RESOURCES_RELATIVE_PATH + CommonConstant.VO_PACKAGE_RELATIVE_NAME,
+                className + ".java"
+        );
+        System.out.println("Generated Vo: " + className + ".java");
+    }
+
+}
